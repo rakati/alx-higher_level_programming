3dc344f2ebacc5032255ea387111201510fcc249
rakati <ouhaddounouredine@gmail.com>
2024-01-04 01:48:17 +0100

    handle case of negative num

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit_s = str(number)[-1]
+if number < 0:
+    last_digit_s = '-' + last_digit_s
 last_digit = int(last_digit_s)
 
 print(f"Last digit of {number} is {last_digit_s}" ,end=' ')
