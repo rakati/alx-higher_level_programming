2cd03822da7fa37248b87ee07ea09ad3de4127db
rakati <ouhaddounouredine@gmail.com>
2024-01-08 15:18:51 +0100

    use loops instead of list comprehension

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    print("\n".join('{:d}'.format(e) for e in my_list[::-1]))
+    for e in my_list[::-1]:
+        print('{:d}'.format(e))
+
+
