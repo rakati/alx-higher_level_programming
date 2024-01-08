8436d7cf2a5fb0b5ad1eaa422707a6ade16f1cd9
rakati <ouhaddounouredine@gmail.com>
2024-01-08 16:30:38 +0100

    use string concate

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string is None:
-        return ""
-    sl = list(my_string)
-    for e in sl:
-        if e in 'cC':
-            sl.remove(e)
-    return "".join(sl)
+    s = ""
+    for e in my_string:
+        if e not in "cC":
+            s += e
+    return s
