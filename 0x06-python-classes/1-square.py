20b01ace14ef82100e1a1a2aa26aae820a839d5e
rakati <ouhaddounouredine@gmail.com>
2024-01-23 14:13:04 +0100

    0x06: recreate Square class with private attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Module for Square class
+'''
+
+
+class Square:
+    '''Class represent square
+    Args:
+        size (int): size of the square side
+    Attributes:
+        _Square__size (int): private attribute represent size of the square
+    '''
+    def __init__(self, size) -> None:
+        self._Square__size = size
