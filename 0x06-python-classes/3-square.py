52bb62d74e508cfbaf7722f6000b12979a737a95
rakati <ouhaddounouredine@gmail.com>
2024-01-23 15:33:18 +0100

    0x06: class square add area method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Module for Square class
+'''
+
+
+class Square:
+    '''Class represent square
+    Args:
+        size (int): size of the square side
+    Attributes:
+        _Square__size (int): private attribute represent size of the square
+    '''
+    def __init__(self, size=0) -> None:
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
+
+    def area(self):
+        '''Return Area of the square'''
+        return self._Square__size * self._Square__size
