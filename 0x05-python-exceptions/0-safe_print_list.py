c7695b205e0aaff0602cf01c878720b50b3fe276
rakati <ouhaddounouredine@gmail.com>
2024-01-25 00:07:32 +0100

    0x05: fix code style

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     for e in range(x):
         try:
             print(my_list[e], end='')
-        except:
+        except IndexError:
             print()
             return e
     print()
