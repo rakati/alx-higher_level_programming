b36d2bb10d6879022e78fe58a292ed4bf931798e
rakati <ouhaddounouredine@gmail.com>
2024-01-31 19:14:53 +0100

    0x08-ex02: fix perimeter method if width or height is 0 perimeter return 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -77,4 +77,6 @@ class Rectangle:
 
     def perimeter(self):
         """Get rectangle perimeter"""
+        if self.width == 0 or self.height == 0:
+            return 0
         return (self.width + self.height) * 2
