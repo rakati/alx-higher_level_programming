ca8239c2cdbc288de99ca20c24a77702024bbc07
rakati <ouhaddounouredine@gmail.com>
2024-02-05 20:24:51 +0100

    0A.ex00: add a func that return list of attrs and methods of passed object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''This Module for rectangle data structure
+Functions:
+    lookup
+'''
+
+
+def lookup(obj):
+    """Return list of attribute and methods of passed objects"""
+    return dir(obj)
