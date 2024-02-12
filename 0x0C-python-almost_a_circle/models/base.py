e2df473d0729f7745a1a612687d779fe06cae6b5
rakati <ouhaddounouredine@gmail.com>
2024-02-12 17:50:44 +0100

    add models as package and add a base class in the models

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+Module with a Base class
+classes:
+--------
+    - Base class
+'''
+
+
+class Base:
+    '''A simple Class that will be used as Base'''
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''
+        Base class constructor
+
+        Parameters:
+        -----------
+            - id: int
+                A integer with default None
+        '''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
