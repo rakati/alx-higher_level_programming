e580d9c0f8d5f5024265b5b12f637e3b138b2556
Nouredine Ouhaddou <ouhaddou.nouredine@gmail.com>
2024-03-14 19:52:12 -0700

    0x0D-ex9: add comment

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,4 @@
+-- Full script that create a table if not exists and insert multiple records to it.
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table
 	(id, name, score)
