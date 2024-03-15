28fc5470e3084f6c8e9cd6704b2a383cf9ce8d41
Nouredine Ouhaddou <ouhaddou.nouredine@gmail.com>
2024-03-14 20:00:12 -0700

    0x0D-ex11:

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- list records score, name of the second table orderted
+-- by score in descending order only with score above 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
