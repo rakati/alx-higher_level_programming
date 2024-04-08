a114c30ef487fbcc07b7d04b37594a6780f338f1
rakati <ouhaddounouredine@gmail.com>
2024-04-08 23:49:59 +0000

    fix printed message for more than one arg

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,4 +2,5 @@
 const { argv } = require('node:process');
 
 if (argv.length === 1) console.log('No argument');
-else console.log('Argument found');
+else if (argv.length === 2) console.log('Argument found');
+else console.log('Arguments found');
