eb4b54d0de918e73326d9599cd2bffcca714029f
rakati <ouhaddounouredine@gmail.com>
2024-04-09 00:03:07 +0000

    fix bug, args contain by default two values execPath and program name

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if (argv.length === 1) console.log('No argument');
-else if (argv.length === 2) console.log('Argument found');
+if (argv.length === 2) console.log('No argument');
+else if (argv.length === 3) console.log('Argument found');
 else console.log('Arguments found');
