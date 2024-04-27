b9b11fc90894c944f9eb3ba16e1eeeb615a956a8
rakati <ouhaddounouredine@gmail.com>
2024-04-27 18:47:04 +0100

    replace spaces with tabulation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     with request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         print("Body response:")
         data = resp.read()
-        print("   - type:", data)
-        print("   - content:", type(data))
-        print("   - utf8 content:", data.decode("utf-8"))
+        print("\t- type:", data)
+        print("\t- content:", type(data))
+        print("\t- utf8 content:", data.decode("utf-8"))
