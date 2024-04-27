9028ac3253a2c3cb29772e1ef1522eb762d1508a
rakati <ouhaddounouredine@gmail.com>
2024-04-27 18:48:51 +0100

    fix bug order of info printed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     with request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         print("Body response:")
         data = resp.read()
-        print("\t- type:", data)
-        print("\t- content:", type(data))
+        print("\t- type:", type(data))
+        print("\t- content:", data)
         print("\t- utf8 content:", data.decode("utf-8"))
